42217f7b034c9c09875e5e30e105b5274763cdc1
Marlou Dev <marlou.dev.app@gmail.com>
2024-10-05 17:52:14 +0800

    comment middleware

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,13 @@ use App\Http\Controllers\SubjectHandledController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 
 Route::resource('class_participation', ClassParticipationController::class);
