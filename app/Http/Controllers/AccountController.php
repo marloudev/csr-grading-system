810f2028946aa3aaf0a0adad84a2a70d2bf2efd0
Marlou Dev <marlou.dev.app@gmail.com>
2024-10-05 18:47:34 +0800

    added addres and DOB

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -33,10 +33,10 @@ class AccountController extends Controller
         $validatedData = $request->validate([
             'user_id' => 'required|unique:users,user_id',  // Corrected 'unique' validation for 'user_id'
             'email' => 'required|email|unique:users,email', // Unique validation for email
-            'address' => 'required|string|max:255',  // Address must be a string with a max length of 255
+            // 'address' => 'required|string|max:255',  // Address must be a string with a max length of 255
             'course_id' => 'max:255',  // Course must be a string with a max length of 255
             'department_id' => 'required',  // Department must be a string with a max length of 255
-            'dob' => 'required|date',  // Date of birth must be a valid date
+            // 'dob' => 'required|date',  // Date of birth must be a valid date
             'fname' => 'required|string|max:255',  // First name must be a string with a max length of 255
             'lname' => 'required|string|max:255',  // Last name must be a string with a max length of 255
             'password' => 'required|string|min:8',  // Password must be a string with a minimum length of 8
@@ -46,10 +46,10 @@ class AccountController extends Controller
         User::create([
             'user_id' => $validatedData['user_id'],
             'email' => $validatedData['email'],
-            'address' => $validatedData['address'],
+            // 'address' => $validatedData['address'],
             'course_id' => $validatedData['course_id'] ?? null,
             'department_id' => $validatedData['department_id'],
-            'dob' => $validatedData['dob'],
+            // 'dob' => $validatedData['dob'],
             'fname' => $validatedData['fname'],
             'lname' => $validatedData['lname'],
             'user_type' => $request->user_type,
