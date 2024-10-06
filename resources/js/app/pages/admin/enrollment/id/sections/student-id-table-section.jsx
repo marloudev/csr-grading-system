6cc5814ce47074f3b32da05f5133b547d04c3b1d
Marlou Dev <marlou.dev.app@gmail.com>
2024-10-06 22:17:46 +0800

    added new build

--- resources/js/app/pages/admin/enrollment/id/sections/student-id-table-section.jsx
+++ resources/js/app/pages/admin/enrollment/id/sections/student-id-table-section.jsx
@@ -16,11 +16,17 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { useSelector } from "react-redux";
 
 function GradeCategoryTable({ categoryName, grades }) {
+
     return (
         <Box className='py-6 w-full' sx={{ margin: 1 }}>
             <Typography variant="h6" gutterBottom component="div">
-                {categoryName}
+                <div className='font-black'>
+                    {categoryName}
+                </div>
             </Typography>
+            <div className='font-bold text-lg'>
+                Prelim
+            </div>
             <Table size="small" aria-label="grades">
                 <TableHead>
                     <TableRow>
@@ -32,7 +38,57 @@ function GradeCategoryTable({ categoryName, grades }) {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {grades?.map((res, i) => (
+                    {grades.prelim?.map((res, i) => (
+                        <TableRow key={i}>
+                            <TableCell>{res.score}</TableCell>
+                            <TableCell>{res.total}</TableCell>
+                            <TableCell>{res.percent}%</TableCell>
+                            <TableCell>{res.remarks}</TableCell>
+                            <TableCell>{res.date}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <div className='font-bold text-lg'>
+                Midterm
+            </div>
+            <Table size="small" aria-label="grades">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Score</TableCell>
+                        <TableCell>Total</TableCell>
+                        <TableCell>Percent</TableCell>
+                        <TableCell>Remarks</TableCell>
+                        <TableCell>Date</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {grades.midterm?.map((res, i) => (
+                        <TableRow key={i}>
+                            <TableCell>{res.score}</TableCell>
+                            <TableCell>{res.total}</TableCell>
+                            <TableCell>{res.percent}%</TableCell>
+                            <TableCell>{res.remarks}</TableCell>
+                            <TableCell>{res.date}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <div className='font-bold text-lg'>
+                Final
+            </div>
+            <Table size="small" aria-label="grades">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Score</TableCell>
+                        <TableCell>Total</TableCell>
+                        <TableCell>Percent</TableCell>
+                        <TableCell>Remarks</TableCell>
+                        <TableCell>Date</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {grades.final?.map((res, i) => (
                         <TableRow key={i}>
                             <TableCell>{res.score}</TableCell>
                             <TableCell>{res.total}</TableCell>
@@ -49,57 +105,95 @@ function GradeCategoryTable({ categoryName, grades }) {
 function Row(props) {
     const { row } = props;
     const [open, setOpen] = React.useState(false);
-    console.log('row',row)
+    const group_examination = row.examination.reduce((acc, exam) => {
+        const key = exam.assessment.toLowerCase();
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(exam);
+        return acc;
+    }, {});
+
+    const group_quiz = row.quiz.reduce((acc, exam) => {
+        const key = exam.assessment.toLowerCase();
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(exam);
+        return acc;
+    }, {});
+
+    const group_project = row.project.reduce((acc, exam) => {
+        const key = exam.assessment.toLowerCase();
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(exam);
+        return acc;
+    }, {});
+
+    const group_class_participation = row.class_participation.reduce((acc, exam) => {
+        const key = exam.assessment.toLowerCase();
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(exam);
+        return acc;
+    }, {});
     return (
         <React.Fragment>
-        {/* The entire row is clickable to toggle */}
-        <TableRow
-            sx={{ '& > *': { borderBottom: 'unset' }, cursor: 'pointer' }}
-            onClick={()=>setOpen(!open)}
-        >
-            <TableCell className="bg-blue-400 w-full">
-                <IconButton aria-label="expand row" size="small">
-                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                </IconButton>
-                {row?.subject_code} ({row.academic_year})
-            </TableCell>
-        </TableRow>
-        <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <div className='flex gap-3 w-full'>
-                        <GradeCategoryTable
-                            categoryName="Examination"
-                            grades={row?.examination ?? []}
-                        />
-                        <GradeCategoryTable
-                            categoryName="Quizzes"
-                            grades={row?.quiz ?? []}
-                        />
-                    </div>
-                    <div className='flex gap-3 w-full'>
-                        <GradeCategoryTable
-                            categoryName="Projects / Assignments"
-                            grades={row?.projects ?? []}
-                        />
-                        <GradeCategoryTable
-                            categoryName="Class Participation"
-                            grades={row?.class_participation ?? []}
-                        />
-                    </div>
+            {/* The entire row is clickable to toggle */}
+            <TableRow
+                sx={{ '& > *': { borderBottom: 'unset' }, cursor: 'pointer' }}
+                onClick={() => setOpen(!open)}
+            >
+                <TableCell className="bg-blue-400 w-full">
+                    <IconButton aria-label="expand row" size="small">
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                    {row?.subject_code} ({row.academic_year})
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <div className='flex gap-3 w-full'>
+                            <GradeCategoryTable
+                                categoryName="Examination"
+                                grades={group_examination ?? []}
+                            />
+                            <GradeCategoryTable
+                                categoryName="Quizzes"
+                                grades={group_quiz ?? []}
+                            />
+                            {/* <GradeCategoryTable
+                                categoryName="Quizzes"
+                                grades={row?.quiz ?? []}
+                            /> */}
+                        </div>
+                        <div className='flex gap-3 w-full'>
+                            <GradeCategoryTable
+                                categoryName="Projects / Assignments"
+                                grades={group_project ?? []}
+                            />
+                            <GradeCategoryTable
+                                categoryName="Class Participation"
+                                grades={group_class_participation ?? []}
+                            />
+                        </div>
 
-                </Collapse>
-            </TableCell>
-        </TableRow>
-    </React.Fragment>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
     );
 }
 
 
 export default function StudentIdTableSection() {
-    
-    const {userGrades} = useSelector((store)=>store.grades)
-    console.log('userGrades',userGrades)
+
+    const { userGrades } = useSelector((store) => store.grades)
+    console.log('userGrades', userGrades)
     return (
         <TableContainer component={Paper}>
             <Table aria-label="collapsible table">
@@ -112,7 +206,7 @@ export default function StudentIdTableSection() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {userGrades.map((row,i) => (
+                    {userGrades.map((row, i) => (
                         <Row key={i} row={row} />
                     ))}
                 </TableBody>
